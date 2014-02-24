31d5784cf30124827c0fcb6eb02322e76f7247f6
George Osterweil <george@georgeosterweil.com>
2014-02-24 13:44:05 -0500

    Fixed compile errors on portscan, added new include

--- Networking/portscan/src/portscan.c
+++ Networking/portscan/src/portscan.c
@@ -1,12 +1,17 @@
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <netinet/in.h>
+#include <stdlib.h>
 #include <stdio.h>
 int main(int argc, char** argv) {
-	char ** ip;
-	int port;
+	char * ip_src;
+    char * ip_dst;
+    ip_dst = malloc(sizeof(struct in_addr));
+	short port;
 	if (argc == 3) {
-		ip  = argv[1];
-		port = strtol(argv[2],NULL,10);
+		ip_src  = argv[1];
+		
+        port = strtol(argv[2],NULL,10);
 		if (port == 0L){
 			printf("Error:Invalid port\n");
 			return 1;
@@ -20,7 +25,8 @@ int main(int argc, char** argv) {
 	int sock = socket(AF_INET,SOCK_STREAM,0);
 	struct sockaddr_in server;
 	server.sin_family = AF_INET;
-	server.sin_port = port;
-	
-	return 0;
+	server.sin_port = htons(port);
+    server.sin_addr.s_addr = inet_pton(AF_INET,ip_src,ip_dst);
+    free(ip_dst);	
+    return 0;
 }
