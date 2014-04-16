c176c82925da478657863ce302d551142d9d228a
George Osterweil <george@georgeosterweil.com>
2014-04-16 12:20:00 -0400

    Refactored and partly restarted portscan project

--- Networking/portscan/src/portscan.c
+++ Networking/portscan/src/portscan.c
@@ -3,30 +3,30 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <netdb.h>
+void error(char *msg){
+    perror(msg);
+    exit(1);
+}
 int main(int argc, char** argv) {
-	char * ip_src;
-    char * ip_dst;
-    ip_dst = malloc(sizeof(struct in_addr));
+    char* addr;
 	short port;
-	if (argc == 3) {
-		ip_src  = argv[1];
-		
+	if (argc == 3) { 
         port = strtol(argv[2],NULL,10);
-		if (port == 0L){
-			printf("Error:Invalid port\n");
-			return 1;
-		}
-
+		if (port == 0L || port < 1){
+		    fprintf(stderr,"Invalid port\n");
+			exit(1);
+        }
+	    addr = argv[1];	
 	}
 	else {
-		printf("Error:Wrong number of arguments\n");
-		return 1;
+		fprintf(stderr,"Wrong number of arguments\n");
+		exit(EXIT_FAILURE);
 	}
-	int sock = socket(AF_INET,SOCK_STREAM,0);
-	struct sockaddr_in server;
-	server.sin_family = AF_INET;
-	server.sin_port = htons(port);
-    server.sin_addr.s_addr = inet_pton(AF_INET,ip_src,ip_dst);
-    free(ip_dst);	
-    return 0;
+    struct addrinfo hints;
+    struct* addrinfo result;
+    memset(&hints,0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    
 }
