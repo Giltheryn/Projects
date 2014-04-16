993cdce7c95ca39a060943ec75b90cdd25b755ab
George Osterweil <george@georgeosterweil.com>
2014-04-16 18:38:24 -0400

    First working version

--- Networking/portscan/src/portscan.c
+++ Networking/portscan/src/portscan.c
@@ -1,32 +1,51 @@
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <netinet/in.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <netdb.h>
-void error(char *msg){
-    perror(msg);
-    exit(1);
-}
+#include <errno.h>
+#include <string.h>
+
+
 int main(int argc, char** argv) {
+    char* addr_in;
     char* addr;
-	short port;
-	if (argc == 3) { 
-        port = strtol(argv[2],NULL,10);
-		if (port == 0L || port < 1){
-		    fprintf(stderr,"Invalid port\n");
-			exit(1);
+    char* port;
+    if (argc == 3) { 
+        port = argv[2];
+        addr_in = argv[1]; 
         }
-	    addr = argv[1];	
-	}
-	else {
-		fprintf(stderr,"Wrong number of arguments\n");
-		exit(EXIT_FAILURE);
-	}
+    else {
+        fprintf(stderr,"Wrong number of arguments\n");
+        exit(EXIT_FAILURE);
+    }
+    int status;    
+    int sock;
     struct addrinfo hints;
-    struct* addrinfo result;
+    struct addrinfo* result;
     memset(&hints,0, sizeof(struct addrinfo));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
+    status = getaddrinfo(argv[1], argv[2], &hints,&result);
+    if(status != 0){
+       fprintf(stderr,"getaddrinfo: %s\n",gai_strerror(status)); 
+       perror(NULL);
+       exit(EXIT_FAILURE);
+    }
+     
+    sock = socket(result->ai_family, result->ai_socktype,result->ai_protocol);
+    if ((connect(sock, result->ai_addr, result->ai_addrlen)) != -1){
+        printf("Port is open\n");
+        close(sock);
+    }
+    else{
+   perror("Error");
+   close(sock);
+   freeaddrinfo(result);
+   exit(1); 
     
+}
+   
+    freeaddrinfo(result);
+    close(sock);
 }
